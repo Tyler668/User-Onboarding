5a2f478ed920d2daf5d9d7ae54120229e433f593
Tyler Gilchrist <tyler668@gmail.com>
2019-08-22 15:32:28 -0400

    added fields

--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -1,7 +1,28 @@
-const Form = () =>{
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Form, Field, withFormik } from "formik";
+import * as Yup from "yup";
 
+const Form = ({ errors, touched, values, status }) =>{
+    const [animals, setAnimals] = useState([]);
+    console.log("this is touched", touched);
+    useEffect(() => {
+      if (status) {
+        setAnimals([...animals, status]);
+      }
+    }, [status]);
 
-    return;
+    return(
+        <div className = "formContainer">
+            <Form>
+                <Field name = "name" type = "text" placeholder = "name"/>
+                <Field name = "email" type = "email" placeholder = "email"/>
+                <Field name = "password" type = "password" placeholder = "password"/>
+                <Field name = "terms" type = "checkbox" />
+            </Form>
+        </div>
+
+    );
 }
 
 

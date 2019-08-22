718a57df8b35b483ff8765b049c141885135d198
Tyler Gilchrist <tyler668@gmail.com>
2019-08-22 16:17:35 -0400

    added extremely aggressive validation schema strings

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Form from "./components/Form";
+import UserForm from "./components/Form";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Form />
+        <UserForm />
       </header>
     </div>
   );
--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -4,27 +4,38 @@ import { Form, Field, withFormik } from "formik";
 import * as Yup from "yup";
 
 const UserForm = ({ errors, touched, values, status }) => {
-    const [animals, setAnimals] = useState([]);
+    const [users, setUsers] = useState([]);
     console.log("this is touched", touched);
     useEffect(() => {
         if (status) {
-            setAnimals([...animals, status]);
+            setUsers([...users, status]);
         }
     }, [status]);
 
     return (
         <div className="formContainer">
             <Form>
-                <Field name="name" type="text" placeholder="name" />
-                <Field name="email" type="email" placeholder="email" />
-                <Field name="password" type="password" placeholder="password" />
+                <Field name="name" type="text" placeholder="Name" />
+                {touched.name && errors.name && (
+                    <p className="error">{errors.name}</p>
+                )}
+
+                <Field name="email" type="email" placeholder="E-mail" />
+                {touched.email && errors.email && (
+                    <p className="error">{errors.email}</p>
+                )}
+
+                <Field name="password" type="password" placeholder="Password (6+ Chars)" />
+                {touched.password && errors.password && (
+                    <p className="error">{errors.password}</p>
+                )}
 
                 <label className="checkbox-container">
                     Terms of Service
-                <Field name="terms" type="checkbox" />
+                <Field name="terms" type="checkbox" checked = {values.terms}/>
                 </label>
 
-                <button>Submit</button>
+                <button type="submit">Submit</button>
             </Form>
         </div>
 
@@ -43,22 +54,22 @@ const FormikUserForm = withFormik({
     },
 
     validationSchema: Yup.object().shape({
-        name: Yup.string().required("Enter a name"),
-        email: Yup.string().required("Enter an email"),
-        password: Yup.string().min(6,"Password must be at least 6 characters long").required("Enter a password"),
-        terms: Yup.boolean().required("Please agree to terms of service")
+        name: Yup.string().required("You forgot to enter a name dude..."),
+        email: Yup.string().required("You forgot an email, c'mon bro."),
+        password: Yup.string().min(6, "Password must be at least 6 characters long, It literally says it on the form my guy ").required("Oh yeah for sure, let's just make an account with no password... smh"),
+        terms: Yup.boolean().required("Read the terms, or don't, just please, click the check box")
     }),
 
     handleSubmit(values, { setStatus }) {
         axios
-          .post("https://reqres.in/api/users/", values)
-          .then(res => {
-            setStatus(res.data);
-          })
-          .catch(err => console.log(err.response));
-      }
+            .post("https://reqres.in/api/users/", values)
+            .then(res => {
+                setStatus(res.data);
+            })
+            .catch(err => console.log(err.response));
+    }
 
 })(UserForm)
 
 
-export default UserForm;
+export default FormikUserForm;
